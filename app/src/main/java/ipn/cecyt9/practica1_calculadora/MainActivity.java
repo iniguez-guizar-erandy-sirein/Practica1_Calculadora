1eefc88191ec99802d9da2770b9bb1492ccd3bfb
erandy <iniguez.guizar.erandy.sirein@gmail.com>
2021-09-01 19:45:52 -0500

    Practica1_Calculadora

--- app/src/main/java/ipn/cecyt9/practica1_calculadora/MainActivity.java
+++ app/src/main/java/ipn/cecyt9/practica1_calculadora/MainActivity.java
@@ -13,9 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
-    Double numero1,resultado;
+    Double numero1, resultado;
     String operador;
 
     public void onClickButtonOne(View miView) {
@@ -96,29 +97,46 @@ public class MainActivity extends AppCompatActivity {
         tv.setText(value + "0");
     }
 
-    public void onClickOperacionCapturaNumero1(View miView){
+    public void onClickOperacionCapturaNumero1(View miView) {
         TextView tv = (TextView) findViewById(R.id.textView);
         numero1 = Double.parseDouble(tv.getText().toString());
         tv.setText("");
     }
 
-    public void onClickSuma(View miView){
-        operador="+";
+    public void onClickSuma(View miView) {
+        operador = "+";
         onClickOperacionCapturaNumero1(miView);
     }
 
-    public void onClickIgual(View miView){
+    public void onClickResta(View miView) {
+        operador = "-";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClicMultiplicacion(View miView) {
+        operador = "*";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+    public void onClickDivision(View miView) {
+        operador = "/";
+        onClickOperacionCapturaNumero1(miView);
+    }
+
+
+    public void onClickIgual(View miView) {
         TextView tv = (TextView) findViewById(R.id.textView);
         numero2 = Double.parseDouble(tv.getText().toString());
 
-        try{
-            if (operador.equals("+")){
+        try {
+            if (operador.equals("+")) {
                 resultado = numero1 + numero2;
             }
             tv.setText(resultado.toString());
-        } catch(NumberFormatException nfe){
+        } catch (NumberFormatException nfe) {
             Toast.makeText(this, "Numero Incorrecto", Toast.LENGTH_SHORT).show();
         }
     }
+
 }
 
